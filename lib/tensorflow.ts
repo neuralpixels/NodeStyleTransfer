4a6092fa65646410454b5f7e78cc683d1eae2686
Jaret Burkett <jaretburkett@gmail.com>
2019-01-29 11:57:31 -0600

    Fixed import

--- lib/tensorflow.ts
+++ lib/tensorflow.ts
@@ -1,6 +1,6 @@
 import {isNode} from './env'
-import * as tf from '@tensorflow/tfjs-node-gpu';
-if(!isNode){
-    throw "Only NodeJS is currently supported"
+import * as tf from '@tensorflow/tfjs-core';
+if(isNode){
+    require('@tensorflow/tfjs-node-gpu')
 }
 export default tf;
